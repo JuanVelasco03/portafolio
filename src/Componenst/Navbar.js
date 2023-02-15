66ffba580efc3cfe6e0f2d5d25addc0a7907071d
JuanVelasco03 <juanyara773@gmail.com>
2023-02-14 21:07:26 -0500

    cambios en el nav

--- src/Componenst/Navbar.js
+++ src/Componenst/Navbar.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'
 import '../css/navbar.css'
 import {BsLinkedin} from 'react-icons/bs'
 import {SiNetlify} from 'react-icons/si'
@@ -5,6 +6,7 @@ import {SiNetlify} from 'react-icons/si'
 
 const Navbar = () => {
   
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const addedPaddingInNavigation = (e, id) => {
     e.preventDefault()
@@ -28,22 +30,32 @@ const Navbar = () => {
       top: rect.top + window.scrollY
     };
   }
+ 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
   
   return (
-    <div className="menu">
-      <nav id="menu">
+    <div className='navbar'>
+
+      <button className='nav_button'  onClick={toggleMenu}>
+         <box-icon name='list-ul' color='#121010' ></box-icon>
+      </button>
+
+      <nav className={`menu ${menuOpen ? 'isActive' : ''}`}>
         <ul className="list">
-          <li className="white"><a href="#0"  onClick={(e) => addedPaddingInNavigation(e, '0')}  className="white">Inicio</a></li>
-          <li className="white"><a href="#1"  onClick={(e) => addedPaddingInNavigation(e, '1')}  className="white">Sobre mi</a></li>
-          <li className="white"><a href="#2"  onClick={(e) => addedPaddingInNavigation(e, '2')} className="white">Habilidades</a></li>
-          <li className="white"><a href="#3"  onClick={(e) => addedPaddingInNavigation(e, '3')} className="white">Proyectos</a></li>
-          <li className="white"><a href="#4"  onClick={(e) => addedPaddingInNavigation(e, '4')} className="white">Contacto</a></li>
+          <li className="white nav_li"><a href="#0"  onClick={(e) => addedPaddingInNavigation(e, '0')}  className="white">Inicio</a></li>
+          <li className="white nav_li"><a href="#1"  onClick={(e) => addedPaddingInNavigation(e, '1')}  className="white">Sobre mi</a></li>
+          <li className="white nav_li"><a href="#2"  onClick={(e) => addedPaddingInNavigation(e, '2')} className="white">Habilidades</a></li>
+          <li className="white nav_li"><a href="#3"  onClick={(e) => addedPaddingInNavigation(e, '3')} className="white">Proyectos</a></li>
+          <li className="white nav_li"><a href="#4"  onClick={(e) => addedPaddingInNavigation(e, '4')} className="white">Contacto</a></li>
           <span className="indicador"></span>
         </ul>
       </nav>
-      <ul className="list2">
-        <li className="white"><a href="/#"><BsLinkedin className="icon"/></a></li>
-        <li className="white"><a href="/#"><SiNetlify className="icon"/></a></li>
+
+      <ul className="list2 none">
+        <li className="white none"><a href="/#"><BsLinkedin className="icon"/></a></li>
+        <li className="white none"><a href="/#"><SiNetlify className="icon"/></a></li>
       </ul>
     </div>
   );
